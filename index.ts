17d7a9e1f3ff66a6622106adf33eaf12a394cf1a
Lilian Kasem <liliankasem@gmail.com>
2017-02-04 11:44:46 -0800

    Tidy up

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const chats = document.getElementById("chats");
 const messages = document.getElementById("messages");
 
 setInterval(() =>
-    fetch("http://9cfef6c5.ngrok.io/api/agent/1")
+    fetch("http://YOUR_BOT_ENDPOINT/api/agent/1")
     .then(response => response.text())
     .then(text => {
         const conversationId = text.replace(/"/g, '');
@@ -23,7 +23,7 @@ const createIframe = (conversationId: string) => {
     const id = `botchat_${conversationId}`;
     const iframe = <HTMLIFrameElement>document.createElement('iframe');
     iframe.id = id;
-    iframe.src = 'botchat?s=DUzMSzP-KqA.cwA.4no.wkhn45S9bnI5oBw6r9-mzO0PAALVBDrNkEMPYMIig2A';
+    iframe.src = 'botchat?s=YOUR_DIRECTLINE_SECRET_ID';
     iframe.width = "320";
     iframe.height = "500";
     iframe.onload = (event: Event) => {
